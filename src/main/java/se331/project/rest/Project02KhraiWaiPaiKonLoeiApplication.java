ca11b5650a6ca8ea5fb5600cc8abc32c62dfbfe3
Pattanachai_n <pondlala33@gmail.com>
2023-10-15 21:41:45 +0700

    initailize

---
+++ src/main/java/se331/project/rest/Project02KhraiWaiPaiKonLoeiApplication.java
@@ -0,0 +1,13 @@
+package se331.project.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project02KhraiWaiPaiKonLoeiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Project02KhraiWaiPaiKonLoeiApplication.class, args);
+    }
+
+}
